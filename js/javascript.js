4ae2f827d9cf012c8d8e3cc4fd1448b5941d2624
Ha Do <meongoccute@gmail.com>
2024-08-19 22:48:21 +0800

    cm

--- js/javascript.js
+++ js/javascript.js
@@ -3,3 +3,26 @@ import { Input, initMDB } from "mdb-ui-kit";
 
 initMDB({ Input });
 
+// navigation
+var OnePageNav = function() {
+    $(".smoothscroll[href^='#'], #ftco-nav ul li a[href^='#']").on('click', function(e) {
+         e.preventDefault();
+
+         var hash = this.hash,
+                 navToggler = $('.navbar-toggler');
+         $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 700, 'easeInOutExpo', function(){
+        window.location.hash = hash;
+      });
+
+
+      if ( navToggler.is(':visible') ) {
+          navToggler.click();
+      }
+    });
+    $('body').on('activate.bs.scrollspy', function () {
+      console.log('nice');
+    })
+};
+OnePageNav();
